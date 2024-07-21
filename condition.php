444f3c1bffc481c265e09bc570165ddba98cb423
itsjeanem <jeanem@duck.com>
2024-07-21 13:50:48 +0000

    imp: edit terms and privacy

--- condition.php
+++ condition.php
@@ -1,28 +1,81 @@
 <?php
-    include('layouts/app.php');
+include('layouts/app.php');
 ?>
 
 <style>
-    div{
+    div {
         text-align: justify;
     }
 </style>
 
-        <!-- Page Header Start -->
-        <div class="container-fluid page-header py-5">
-            <div class="container text-center py-5">
-                <h1 class="display-2 text-white mb-4 animated slideInDown">Conditions d'utilisation</h1>
-                <p class="text-white">En utilisant notre site web, vous acceptez de respecter et d'etre lie par les presentes Conditions d'utilisation. Veuillez les lire attentivement</p>
-            </div>
-        </div>
-        <!-- Page Header End -->
-
-
-        <!-- Terms section -->
-<section class="mt-5 px-5">
-    <div>
-        <h3>1. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt cupiditate veniam quia, ea excepturi enim!</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam quia iure porro nostrum laborum error enim tenetur excepturi cupiditate officiis sequi tempore beatae, quo quibusdam perferendis. Eum perspiciatis at tempore eos in placeat ab quae, iure assumenda voluptatum voluptatem et distinctio fuga praesentium inventore incidunt officia voluptate quisquam repellat? Amet fuga numquam laborum atque rerum, dignissimos saepe ad debitis dolor blanditiis dolore adipisci deserunt quam molestias, autem et expedita soluta! Doloribus, dolore. Excepturi suscipit nisi facilis reprehenderit dicta eligendi aperiam expedita, architecto illum iusto necessitatibus ipsum doloribus asperiores libero fugiat laboriosam! Sed, incidunt. Sapiente repellendus, fuga culpa obcaecati facere similique.</p>
+<!-- Page Header Start -->
+<div class="container-fluid page-header py-5">
+    <div class="container text-center py-5">
+        <h1 class="display-2 text-white mb-4 animated slideInDown">Conditions d'utilisation</h1>
+        <p class="text-white">En utilisant notre site web, vous acceptez de respecter et d'être lié par les présentes Conditions Générales d'Utilisation (CGU). Veuillez les lire attentivement.</p>
+    </div>
 </div>
+<!-- Page Header End -->
 
+
+<!-- Terms section -->
+<section class="my-5" style="margin-left: 10%; margin-right: 10%;">
+    <div>
+        <h3>1. Acceptation des Conditions</h3>
+        <p>En vous inscrivant sur notre site ou en utilisant nos services, vous reconnaissez avoir lu, compris et accepté les présentes CGU dans leur intégralité.</p>
+    </div>
+
+    <div>
+        <h3>2. Accès au Site</h3>
+        <p>Notre site est accessible gratuitement à tout utilisateur disposant d'un accès à internet. Les frais liés à l'accès au site (matériel, logiciels, accès internet, etc.) sont à la charge de l'utilisateur.</p>
+    </div>
+
+    <div>
+        <h3>3. Inscription et Compte Utilisateur</h3>
+        <p>Pour utiliser certains services de notre plateforme, vous devez créer un compte en fournissant des informations exactes et à jour.</p>
+        <p>Vous êtes responsable de la confidentialité de votre identifiant et de votre mot de passe, ainsi que de toutes les activités effectuées depuis votre compte.</p>
+    </div>
+
+    <div>
+        <h3>4. Services Offerts</h3>
+        <p>Notre plateforme permet aux artisans de promouvoir leurs services et aux clients de trouver et contacter des artisans. Nous ne sommes pas responsables de la qualité des services fournis par les artisans inscrits.</p>
+    </div>
+
+
+    <div>
+        <h3>5. Obligations des Utilisateurs</h3>
+        <p>Vous vous engagez à utiliser notre site conformément aux présentes CGU et aux lois en vigueur.</p>
+        <p>Vous ne devez pas utiliser notre plateforme à des fins illégales ou non autorisées.</p>
+    </div>
+
+    <div>
+        <h3>6. Obligations des Artisans</h3>
+        <p>Les artisans s'engagent à fournir des informations exactes et complètes lors de leur inscription et à maintenir ces informations à jour</p>
+        <p>Les artisans sont responsables de la qualité des services qu'ils proposent et des engagements pris envers les clients.</p>
+    </div>
+
+    <div>
+        <h3>7. Modération et Résiliation</h3>
+        <p>Nous nous réservons le droit de suspendre ou de résilier le compte de tout utilisateur ou artisan qui ne respecterait pas les présentes CGU.</p>
+        <p>En cas de violation grave des CGU, nous pouvons prendre des mesures légales appropriées.</p>
+    </div>
+
+    <div>
+        <h3>8. Protection des Données Personnelles</h3>
+        <p>Nous nous engageons à protéger vos données personnelles conformément à notre Politique de Confidentialité. En utilisant notre site, vous consentez à la collecte et à l'utilisation de vos données personnelles comme décrit dans cette politique.</p>
+    </div>
+
+    <div>
+        <h3>9. Limitation de Responsabilité</h3>
+        <p>Nous déclinons toute responsabilité en cas de dommages directs ou indirects résultant de l'utilisation de notre site ou des services proposés par les artisans inscrits.</p>
+    </div>
+
+    <div>
+        <h3>10. Modification des CGU</h3>
+        <p>Nous nous réservons le droit de modifier les présentes CGU à tout moment. Les modifications seront effectives dès leur publication sur notre site. Il vous incombe de consulter régulièrement les CGU pour vous tenir informé des éventuelles modifications.</p>
+    </div>
 </section>
+
+<?php
+include('footer.php');
+?>
--- politique.php
+++ politique.php
@@ -1,28 +1,61 @@
 <?php
-    include('layouts/app.php');
+include('layouts/app.php');
 ?>
 
 <style>
-    div{
+    div {
         text-align: justify;
     }
 </style>
 
-        <!-- Page Header Start -->
-        <div class="container-fluid page-header py-5">
-            <div class="container text-center py-5">
-                <h1 class="display-2 text-white mb-4 animated slideInDown">Politique de confidentialite</h1>
-                <p class="text-white">Nous attachons une grande importance a la protection de votre vie privee. Cette politique de confidentialite decrit comment nous collections, utilisons et protegeons vos donnees personnelles.</p>
-            </div>
-        </div>
-        <!-- Page Header End -->
-
-
-        <!-- Terms section -->
-<section class="mt-5 px-5">
-    <div>
-        <h3>1. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt cupiditate veniam quia, ea excepturi enim!</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam quia iure porro nostrum laborum error enim tenetur excepturi cupiditate officiis sequi tempore beatae, quo quibusdam perferendis. Eum perspiciatis at tempore eos in placeat ab quae, iure assumenda voluptatum voluptatem et distinctio fuga praesentium inventore incidunt officia voluptate quisquam repellat? Amet fuga numquam laborum atque rerum, dignissimos saepe ad debitis dolor blanditiis dolore adipisci deserunt quam molestias, autem et expedita soluta! Doloribus, dolore. Excepturi suscipit nisi facilis reprehenderit dicta eligendi aperiam expedita, architecto illum iusto necessitatibus ipsum doloribus asperiores libero fugiat laboriosam! Sed, incidunt. Sapiente repellendus, fuga culpa obcaecati facere similique.</p>
+<!-- Page Header Start -->
+<div class="container-fluid page-header py-5">
+    <div class="container text-center py-5">
+        <h1 class="display-2 text-white mb-4 animated slideInDown">Politique de confidentialité</h1>
+        <p class="text-white">Nous attachons une grande importance à la protection de votre vie privée. Cette politique de confidentialité décrit comment nous collectons, utilisons, et protégeons vos données personnelles.</p>
+    </div>
 </div>
+<!-- Page Header End -->
 
+
+<!-- Terms section -->
+<section class="my-5" style="margin-left: 10%; margin-right: 10%;">
+    <div>
+        <h3>1. Informations Collectées</h3>
+        <p>Lors de votre inscription ou de l'utilisation de nos services, nous pouvons collecter des informations personnelles telles que votre nom, adresse, adresse e-mail, numéro de téléphone, etc.</p>
+    </div>
+
+    <div>
+        <h3>2. Utilisation des Informations</h3>
+        <p> <span class="fw-bold">Fournir nos services :</span> Nous utilisons vos informations pour créer et gérer votre compte, faciliter votre utilisation de notre plateforme, et répondre à vos demandes.</p>
+        <p> <span class="fw-bold">Amélioration de nos services :</span> Nous pouvons utiliser vos informations pour comprendre comment vous utilisez notre site et pour améliorer nos services.</p>
+        <p> <span class="fw-bold">Communication :</span> Nous pouvons utiliser vos informations pour vous envoyer des mises à jour, des newsletters, ou des informations promotionnelles. Vous pouvez choisir de ne plus recevoir ces communications à tout moment.</p>
+    </div>
+
+    <div>
+        <h3>3. Protection des Données</h3>
+        <p>Nous mettons en œuvre des mesures de sécurité techniques et organisationnelles appropriées pour protéger vos informations contre l'accès non autorisé, l'altération, la divulgation ou la destruction. Cependant, aucune méthode de transmission sur Internet ou de stockage électronique n'est totalement sécurisée.</p>
+    </div>
+
+    <div>
+        <h3>4. Vos Droits</h3>
+        <p> <span class="fw-bold">Accès :</span> Vous avez le droit d'accéder à vos informations personnelles que nous détenons.</p>
+        <p> <span class="fw-bold">Correction :</span> Vous avez le droit de demander la correction de toute information personnelle inexacte ou incomplète.</p>
+        <p> <span class="fw-bold">Suppression :</span> Vous avez le droit de demander la suppression de vos informations personnelles, sous réserve de certaines exceptions légales.</p>
+        <p> <span class="fw-bold">Opposition :</span> Vous avez le droit de vous opposer à certains traitements de vos informations personnelles.</p>
+    </div>
+
+    <div>
+        <h3>4. Cookies et Technologies Similaires</h3>
+        <p>Nous utilisons des cookies et des technologies similaires pour améliorer votre expérience sur notre site. Vous pouvez configurer votre navigateur pour refuser les cookies, mais cela peut limiter votre utilisation de certaines fonctionnalités de notre site.</p>
+    </div>
+
+    <div>
+        <h3>5. Modification de la Politique de Confidentialité</h3>
+        <p>Nous nous réservons le droit de modifier cette politique de confidentialité à tout moment. Les modifications seront effectives dès leur publication sur notre site. Il vous incombe de consulter régulièrement cette politique pour vous tenir informé des éventuelles modifications.
+    </div>
 </section>
+
+<?php
+include('footer.php');
+?>
